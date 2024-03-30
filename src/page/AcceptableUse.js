843ab1db64c7d989fd1c4f64e4a1d077343634b3
Hjindal-dev <jindal.heenu@gmail.com>
2024-03-29 23:58:08 -0400

    AcceptableUse and ConjunctionASSessment updated

--- src/page/AcceptableUse.js
+++ src/page/AcceptableUse.js
@@ -1,69 +1,77 @@
 import React from 'react';
-import { Typography, Card, CardContent, CardHeader } from '@mui/material';
-import { Container, Accordion } from 'react-bootstrap';
+import { Typography, Container, Grid, Card, CardContent, CardHeader } from '@mui/material';
+import { Accordion } from 'react-bootstrap';
 
 const AcceptableUsagePolicy = () => {
   return (
     <Container>
       <Typography variant="h2" component="h2" gutterBottom>Acceptable Usage Policy</Typography>
 
-      <Card sx={{ marginBottom: '1rem', backgroundColor: "#99A3B1" }}>
-      <CardHeader title="Purpose" />
-        <CardContent>
-          <Typography variant="body1">
-            EOSCA is designed to promote space situational awareness and mitigate collision risks in near-Earth orbit. The acceptable usage policy was created to ensure the safe, responsible, and ethical use of the EOSCA web application by all users.
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ marginBottom: '1rem', backgroundColor: "#99A3B1" }}>
-      <CardHeader title="User Responsibilities" />
-        <CardContent>
-          <Typography variant="body1">
-            Users must use EOSCA for lawful purposes only and must not engage in any activities that violate the principles of transparency, integrity, and ethical usage outlined in the EOSCA ethics and sustainability considerations.
-          </Typography>
-          <Typography variant="body1">
-            Users are prohibited from using EOSCA for malicious purposes, such as targeting specific satellites.
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ marginBottom: '1rem', backgroundColor: "#99A3B1" }}>
-        <CardHeader title="Transparency and Ethical Usage" />
-        <CardContent>
-          <Typography variant="body1">
-            EOSCA is committed to transparency in design and usage. Users can expect clear documentation of data sources, algorithms used, and methodologies for risk assessment to ensure trust and verify the information provided by the application.
-          </Typography>
-          <Typography variant="body1">
-            Users are encouraged to use EOSCA in an ethical manner, respecting the principles of transparency, and integrity in space operations.
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ marginBottom: '1rem', backgroundColor: "#99A3B1" }}>
-      <CardHeader title ="Security"/>
-        <CardContent>
-          <Typography variant="body1">
-            Security controls are implemented to ensure the confidentiality, integrity, and availability of the data and the runtime environment of EOSCA.
-          </Typography>
-          <Typography variant="body1">
-            Users are prohibited from attempting to bypass any security controls or engage in any activities that compromise the security of the EOSCA system.
-          </Typography>
-        </CardContent>
-      </Card>
-
-      <Card sx={{ marginBottom: '1rem', backgroundColor: "#99A3B1" }}>
-      <CardHeader title = "Accessibility Design" />
-        <CardContent>
-          <Typography variant="body1">
-            EOSCA prioritizes a user-friendly interface with an easy to navigate system and simplistic presentation of data. This provides flexibility with regards to access for users with different abilities.
-          </Typography>
-          <Typography variant="body1">
-            Users are encouraged to provide feedback on the user interface and experience to facilitate continual improvement and ensure human-centric design principles are upheld.
-          </Typography>
-        </CardContent>
-      </Card>
-
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ backgroundColor: "#99A3B1" }}>
+            <CardHeader title="Purpose" />
+            <CardContent>
+              <Typography variant="body1">
+                EOSCA is designed to promote space situational awareness and mitigate collision risks in near-Earth orbit. The acceptable usage policy was created to ensure the safe, responsible, and ethical use of the EOSCA web application by all users.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ backgroundColor: "#99A3B1" }}>
+            <CardHeader title="User Responsibilities" />
+            <CardContent>
+              <Typography variant="body1">
+                Users must use EOSCA for lawful purposes only and must not engage in any activities that violate the principles of transparency, integrity, and ethical usage outlined in the EOSCA ethics and sustainability considerations.
+              </Typography>
+              <Typography variant="body1">
+                Users are prohibited from using EOSCA for malicious purposes, such as targeting specific satellites.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ backgroundColor: "#99A3B1" }}>
+            <CardHeader title="Transparency and Ethical Usage" />
+            <CardContent>
+              <Typography variant="body1">
+                EOSCA is committed to transparency in design and usage. Users can expect clear documentation of data sources, algorithms used, and methodologies for risk assessment to ensure trust and verify the information provided by the application.
+              </Typography>
+              <Typography variant="body1">
+                Users are encouraged to use EOSCA in an ethical manner, respecting the principles of transparency, and integrity in space operations.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ backgroundColor: "#99A3B1" }}>
+            <CardHeader title="Security" />
+            <CardContent>
+              <Typography variant="body1">
+              Security controls are implemented to ensure the confidentiality, integrity, and availability of the data and the runtime environment of EOSCA.
+              </Typography>
+              <Typography variant="body1">
+              Users are prohibited from attempting to bypass any security controls or engage in any activities that compromise the security of the EOSCA system.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card sx={{ backgroundColor: "#99A3B1" }}>
+            <CardHeader title="Accessibility Design" />
+            <CardContent>
+              <Typography variant="body1">
+              EOSCA prioritizes a user-friendly interface with an easy to navigate system and simplistic presentation of data. This provides flexibility with regards to access for users with different abilities.
+              </Typography>
+              <Typography variant="body1">
+              Users are encouraged to provide feedback on the user interface and experience to facilitate continual improvement and ensure human-centric design principles are upheld.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+      <p></p>
       <Accordion className="mb-5">
         <Accordion.Item eventKey="0">
           <Accordion.Header>Reporting Violations</Accordion.Header>
--- src/page/ConjunctionAssessment.js
+++ src/page/ConjunctionAssessment.js
@@ -1,52 +1,90 @@
 import React from 'react';
-import { Container, Card } from 'react-bootstrap';
+import { Container, Typography, Grid, Card, CardContent, CardHeader } from '@mui/material';
 
 const ConjunctionAssessment = () => {
   return (
     <Container>
-      <h2 className="mt-5 mb-4">Conjunction Assessment in Space Operations</h2>
-      
-      <Card className="mb-4">
-        <Card.Body>
-          <Card.Title>NASA's Best Practices Handbook</Card.Title>
-          <Card.Text>
-            NASA's Spacecraft Conjunction Assessment and Collision Avoidance Best Practices Handbook outlines a structured approach to on-orbit collision avoidance, consisting of three phases:
-            <ul style = {{textAlign: 'left'}}>
-              <li><strong>Conjunction Assessment Screening:</strong> Identifying close approaches between a primary satellite and other space objects.</li>
-              <li><strong>Conjunction Risk Assessment:</strong> Examining close approaches to determine collision risk and the need for mitigation.</li>
-              <li><strong>Conjunction Mitigation:</strong> Developing and executing mitigation actions to reduce collision risk.</li>
-            </ul>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      
-      <Card>
-        <Card.Body>
-          <Card.Title>EOSCA's Solution Methodology</Card.Title>
-          <Card.Text style = {{textAlign: 'left'}}>
-            EOSCA (Earth Orbit Spacecraft Collision Avoidance) focuses on performing the first two phases of conjunction assessment: screening and risk assessment. Leveraging a web application, EOSCA utilizes standardized astrodynamics algorithms, notably the SGP4 model, to interpret Two-Line Element set (TLE) data obtained from space-track.org. This data, backed by the 18th Space Defense Squadron of the US government, provides near real-time positional and velocity information for both satellites and debris objects. The 18th Space Control Squadron (SPCS) is responsible for providing space situational awareness and tracking objects in orbit to prevent potential collisions. 
-          </Card.Text>
-          <Card.Text style = {{textAlign: 'left'}}>
-          The application predicts the future positions and trajectories of these objects, refining state vectors with observational data to account for uncertainties. It computes the Euclidean distance between objects to determine collision probability. A risk assessment algorithm categorizes potential encounters into various risk levels, ranging from low to critical, based on collision probability.
-          </Card.Text>
-          <Card.Text style = {{textAlign: 'left'}}>
-          The Euclidean distance is calculated over a 24-hour period and incorporates the predicted positions of objects during this time frame. The risk assessment algorithm categorizes potential encounters into various risk levels, ranging from low to critical, based on collision probability.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      
-      <h3 className="mt-4 mb-3">Key Components of EOSCA's Approach:</h3>
-      <ul style = {{textAlign: 'left'}}>
-        <li><strong>SGP4 Model:</strong> Utilized to interpret TLE data and predict object trajectories.</li>
-        <li><strong>State Vector Refinement:</strong> Incorporates observational data to improve accuracy and account for uncertainties.</li>
-        <li><strong>Collision Probability Computation:</strong> Calculates the likelihood of collision based on object trajectories.</li>
-        <li><strong>Risk Assessment Algorithm:</strong> Categorizes potential encounters into different risk levels.</li>
-        <li><strong>Web Application Interface:</strong> Provides users with access to real-time data and risk assessments.</li>
-      </ul>
-      
-      <p className="mt-4" style = {{textAlign: 'left'}}>
-        Conjunction assessment is essential for safeguarding assets in space and maintaining the integrity of space operations. By implementing robust methodologies and leveraging advanced technologies, solutions like EOSCA play a crucial role in enhancing space situational awareness and mitigating collision risks, contributing to the sustainability of space activities.
-      </p>
+      <Typography variant="h2" component="h2">Conjunction Assessment in Space Operations</Typography>
+      <p></p>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <Card sx={{ backgroundColor: "#99A3B1", height: '100%', textAlign: "left" }} className="mb-4">
+            <CardHeader title="NASA's Best Practices Handbook" />
+            <CardContent>
+              <Typography variant="body1">
+                NASA's Spacecraft Conjunction Assessment and Collision Avoidance Best Practices Handbook outlines a structured approach to on-orbit collision avoidance, consisting of three phases:
+              </Typography>
+              <p></p>
+              <ul style={{ textAlign: 'left' }}>
+                <li><strong>Conjunction Assessment Screening:</strong> Identifying close approaches between a primary satellite and other space objects.</li>
+                <li><strong>Conjunction Risk Assessment:</strong> Examining close approaches to determine collision risk and the need for mitigation.</li>
+                <li><strong>Conjunction Mitigation:</strong> Developing and executing mitigation actions to reduce collision risk.</li>
+              </ul>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card sx={{ backgroundColor: "#99A3B1", height: '100%', textAlign: "left" }}>
+            <CardHeader title="EOSCA's Solution Methodology" />
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1">
+                    EOSCA (Earth Orbit Spacecraft Collision Avoidance) focuses on performing the first two phases of conjunction assessment: screening and risk assessment. Leveraging a web application, EOSCA utilizes standardized astrodynamics algorithms, notably the SGP4 model, to interpret Two-Line Element set (TLE) data obtained from space-track.org. This data, backed by the 18th Space Defense Squadron of the US government, provides near real-time positional and velocity information for both satellites and debris objects. The 18th Space Control Squadron (SPCS) is responsible for providing space situational awareness and tracking objects in orbit to prevent potential collisions.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    The application predicts the future positions and trajectories of these objects, refining state vectors with observational data to account for uncertainties. It computes the Euclidean distance between objects to determine collision probability. A risk assessment algorithm categorizes potential encounters into various risk levels, ranging from low to critical, based on collision probability.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography>
+                    The Euclidean distance is calculated over a 24-hour period and incorporates the predicted positions of objects during this time frame. The risk assessment algorithm categorizes potential encounters into various risk levels, ranging from low to critical, based on collision probability.
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={12}>
+          <Card sx={{ backgroundColor: "#99A3B1", height: '100%' }}>
+            <CardContent>
+              <Typography variant="h3" className="mt-4 mb-3">Key Components of EOSCA's Approach:</Typography>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    <strong>SGP4 Model:</strong> Utilized to interpret TLE data and predict object trajectories.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    <strong>State Vector Refinement:</strong> Incorporates observational data to improve accuracy and account for uncertainties.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    <strong>Collision Probability Computation:</strong> Calculates the likelihood of collision based on object trajectories.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    <strong>Risk Assessment Algorithm:</strong> Categorizes potential encounters into different risk levels.
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Typography variant="body1" style={{ textAlign: 'left' }}>
+                    <strong>Web Application Interface:</strong> Provides users with access to real-time data and risk assessments.
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Typography variant="body1" className="mt-4" style={{ textAlign: 'left' }}>
+                Conjunction assessment is essential for safeguarding assets in space and maintaining the integrity of space operations. By implementing robust methodologies and leveraging advanced technologies, solutions like EOSCA play a crucial role in enhancing space situational awareness and mitigating collision risks, contributing to the sustainability of space activities.
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
